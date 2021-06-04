54b094092a2ca63e0349915b899f54c77a44e522
Ahmed Elsaid A.I <abrabou@yandex.ru>
2021-06-04 14:55:19 +0300

    Part 2

--- Lists/Linked_list.java
+++ Lists/Linked_list.java
@@ -10,6 +10,9 @@ package Linked_Lists;
  ***/
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 class customer {
     private String name;
@@ -54,13 +57,84 @@ public class Linked_list {
         intList.add(3);
         intList.add(4);
         printArrayList(intList);
+        // adding a new index in  ArrayList
         intList.add(1, 2);
         printArrayList(intList);
+// output
+//        1: 1
+//        2: 3
+//        3: 4
+
+//        1: 1
+//        2: 2
+//        3: 3
+//        4: 4
     }
 
+    // printing ArrayList
     public static void printArrayList(ArrayList<Integer> array) {
         for (int i = 0; i < array.size(); i++) {
             System.out.println((i + 1) + ": " + array.get(i));
         }
+    }
+}
+
+// second compare
+class demo {
+    public static void main(String[] args) {
+        LinkedList<String> placeToVisit = new LinkedList<String>();
+        addInOrder(placeToVisit, "Sydney");
+        addInOrder(placeToVisit, "Brisbane");
+        addInOrder(placeToVisit, "Perth");
+        addInOrder(placeToVisit, "Adelaide");
+        addInOrder(placeToVisit, "Canberra");
+        addInOrder(placeToVisit, "Darwin");
+        addInOrder(placeToVisit, "Egypt");
+        printLinkedList(placeToVisit);
+        addInOrder(placeToVisit, "USA");
+        addInOrder(placeToVisit, "Samara");
+        printLinkedList(placeToVisit);
+//        placeToVisit.add("Sydney");
+//        placeToVisit.add("Melbourne");
+//        placeToVisit.add("Brisbane");
+//        placeToVisit.add("Perth");
+//        placeToVisit.add("Canberra");
+//        placeToVisit.add("Adelaide");
+//        placeToVisit.add("Darwin");
+//        printLinkedList(placeToVisit);
+//        placeToVisit.add(2,"Egypt");
+//        printLinkedList(placeToVisit);
+//        placeToVisit.remove(4);
+//        printLinkedList(placeToVisit);
+    }
+
+    public static boolean addInOrder(LinkedList<String> array, String newCity) {
+        ListIterator<String> stringListIterator = array.listIterator();
+        while (stringListIterator.hasNext()) {
+            int compirition = stringListIterator.next().compareTo(newCity);
+            if (compirition == 0) {
+                // equal , don't add
+                System.out.println(newCity + " is already included as a destination.");
+                return false;
+            } else if (compirition > 0) {
+                // new city should appear before this one.
+                // Brisbane --> Adelaide
+                stringListIterator.previous();
+                stringListIterator.add(newCity);
+                return true;
+            } else if (compirition < 0) {
+                // move on next city
+            }
+        }
+        stringListIterator.add(newCity);
+        return true;
+    }
+
+    public static void printLinkedList(LinkedList<String> array) {
+        Iterator<String> i = array.iterator();
+        while (i.hasNext()) {
+            System.out.println("the next place is: " + i.next());
+        }
+        System.out.println("===================================");
     }
 }
